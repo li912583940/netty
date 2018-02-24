dd42c8fa7f797d5ec7cf4fe52aba555bbc140cd0
lixiaotian <lixiaotian@chinamobo.com>
2018-02-24 15:33:52 +0800

    myNetty

---
+++ myNetty/src/main/java/com/sl/ue/Application.java
@@ -0,0 +1,20 @@
+package com.sl.ue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述 []
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * 功能描述 []
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Application.class, args);
+	}
+
+}
